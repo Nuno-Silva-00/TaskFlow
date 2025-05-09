591692373f2a2215db6be98d4d1f0c1e6a693b45
Jose Silva (CTW) <nuno.silva.00@pm.me>
2025-05-09 19:48:31 +0100

    Create navbar

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  config = [
+    {
+      name: 'Board',
+      path: '/board',
+      img: 'assets/kanban.svg',
+      imgAlt: 'Board icon',
+    },
+    {
+      name: 'Inventory',
+      path: '/inventory',
+      img: 'assets/inventory.svg',
+      imgAlt: 'Inventory icon',
+    },
+    {
+      name: 'Settings',
+      path: '/settings',
+      img: 'assets/settings.svg',
+      imgAlt: 'Settings icon',
+    },
+    {
+      name: 'Admin Panel',
+      path: '/admin',
+      img: 'assets/admin.svg',
+      imgAlt: 'Admin icon',
+    },
+  ];
 }
